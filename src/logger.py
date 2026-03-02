608b4016fc36c9bd27e0bc60d311d27cc80185ad
Nikhil lath <lathnikhil2@gmail.com>
2026-03-02 20:13:56 +0530

    data_ingestion

--- src/logger.py
+++ src/logger.py
@@ -1,18 +1,19 @@
 import logging
-
-import os 
+import os
 from datetime import datetime
 
+# Get project root directory (one level above src)
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 
-LOG_FILE=f"{datetime.now().strftime('%m_%d_%y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(logs_path,exist_ok=True)
+# Logs folder outside src
+LOG_DIR = os.path.join(BASE_DIR, "logs")
+os.makedirs(LOG_DIR, exist_ok=True)
 
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%y_%H_%M_%S')}.log"
+LOG_FILE_PATH = os.path.join(LOG_DIR, LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
     format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
-
 )
