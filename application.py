fab1013d627ff1ee14690abdbec96eaaf259048e
Nikhil lath <lathnikhil2@gmail.com>
2026-03-05 17:40:27 +0530

    final

---
+++ application.py
@@ -0,0 +1,41 @@
+from flask import Flask,request,render_template
+import numpy as np
+import pandas as pd
+
+
+from src.pipeline.predict_pipeline import CustomData,Predictpipeline
+
+from sklearn.preprocessing import StandardScaler
+
+
+application=Flask(__name__)
+
+app=application
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict_data',methods=['Get','POST'])
+def predict_datapoint():
+    if request.method=='GET':
+        return render_template('predict.html')
+    
+    else:
+        data=CustomData(
+            gender=request.form.get("gender"),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            average=float(request.form.get('average'))
+        )
+        pred_data=data.get_data_as_dataframe()
+        print (pred_data) 
+        predict_pipeline=Predictpipeline()
+        results=predict_pipeline.predict(pred_data)
+
+        return render_template('predict.html',results=results[0])
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")
--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,8 @@ import sys
 import dill
 from sklearn.metrics import r2_score
 from src.exception import CustomException
+
+from sklearn.model_selection import GridSearchCV
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -14,28 +16,43 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-
-
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, params):
     try:
-        report={}
+        report = {}
 
-        for i in range(len(models)):
-            model=list(models.values())[i]
+        for model_name, model in models.items():
 
-            model.fit(X_train,y_train)
+            param = params[model_name]
 
-            y_train_pred=model.predict(X_train)
-            y_test_pred=model.predict(X_test)
+            gs = GridSearchCV(model, param, cv=3)
+            gs.fit(X_train, y_train)
 
-            train_model_r2_score=r2_score(y_train,y_train_pred)
-            test_model_r2_score=r2_score(y_test,y_test_pred)
+            # apply best parameters
+            model.set_params(**gs.best_params_)
 
-            report[list(models.keys())[i]]=test_model_r2_score
+            # train model again
+            model.fit(X_train, y_train)
 
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_score = r2_score(y_train, y_train_pred)
+            test_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_score
 
         return report
 
     except Exception as e:
+        raise CustomException(e, sys)
+    
 
+
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file_obj:
+            return dill.load(file_obj)
+        
+    except Exception as e:
         raise CustomException(e,sys)
+      
